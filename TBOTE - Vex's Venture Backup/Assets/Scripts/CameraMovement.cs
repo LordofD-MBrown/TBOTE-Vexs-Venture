5c3ff29232812586d372c2a63423ac48da9b18c9
LordofD-MBrown <44680178+LordofD-MBrown@users.noreply.github.com>
2018-11-01 14:32:58 -0400

    Inital Commit Setting - MB
    Inital Commit Transfered from my version, this is a duplicate, I will be keeping a backup on my hardrive.

---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float speedLeftRight = 2f;
+    public float speedUpDown = 2f;
+    private float leftRight = 0f;
+    private float upDown = 0f;
+    GameObject character;
+    // Use this for initialization
+    void Start()
+    {
+
+        character = GameObject.Find("Player");
+    }
+
+
+    void Update()
+    {
+        //RenderSettings.fogDensity;
+        Cursor.lockState = CursorLockMode.Locked;
+        leftRight += speedLeftRight * Input.GetAxis("Mouse X");
+        upDown += speedUpDown * Input.GetAxis("Mouse Y");
+
+        character.transform.eulerAngles = new Vector3(0f, leftRight, 0f);
+    }
+
+    void LateUpdate()
+    {
+        transform.eulerAngles = new Vector3(upDown * -1, leftRight, 0f);
+    }
+}
