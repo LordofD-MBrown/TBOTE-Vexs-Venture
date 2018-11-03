dda6a1adfb791808b6df00a6109fbef042a1c03d
Matthew Brown <matthew.t.brown.mtb@gmail.com>
2018-11-03 14:29:05 -0400

    Created Torch Script
    -MB

---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/NightScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        RenderSettings.fog = true;
+        RenderSettings.fogDensity = 5;
+	}
+
+
+
+}
---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/Torch Script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchScript : MonoBehaviour {
+
+    public GameObject torch;
+    public GameObject flame;
+    double size_of_flame = 0.15;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+}
