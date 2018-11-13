487f776b2d33d80975a33a5bd9b71f23ad0a2198
ahaynes34724 <haynes.avery@yahoo.com>
2018-11-13 14:57:06 -0500

    Random Postion for Knife
    AH

--- TBOTE - Vex's Venture Backup/Assets/Scripts/KnifeSpawn.cs
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/KnifeSpawn.cs
@@ -2,15 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KnifeSpawn : MonoBehaviour {
+public class KnifeSpawn : MonoBehaviour
+{
+    public GameObject KnifePos1;
+    public GameObject KnifePos2;
+    public GameObject KnifePos3;
+    public GameObject KnifePos4;
+    public GameObject KnifePos5;
+    public GameObject KnifePos6;
 
-	// Use this for initialization
-	void Start () {
-		
+    Random rnd = new Random();
+    
+    // Use this for initialization
+    void Start ()
+    {
+        int getPos = Random.Range(0, 7);
+
+        if (getPos == 0)
+        {
+            KnifePos1.SetActive(true);
+        }
+        if (getPos == 1)
+        {
+            KnifePos2.SetActive(true);
+        }
+        if (getPos == 2)
+        {
+            KnifePos3.SetActive(true);
+        }
+        if (getPos == 3)
+        {
+            KnifePos4.SetActive(true);
+        }
+        if (getPos == 4)
+        {
+            KnifePos5.SetActive(true);
+        }
+        if (getPos == 5)
+        {
+            KnifePos6.SetActive(true);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/NightVisionSpawn.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NightVision : MonoBehaviour
+{
+
+    public GameObject NightVisionPos1;
+    public GameObject NightVisionPos2;
+    public GameObject NightVisionPos3;
+    public GameObject NightVisionPos4;
+    public GameObject NightVisionPos5;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        int getPos = Random.Range(0, 6);
+
+        if (getPos == 0)
+        {
+            NightVisionPos1.SetActive(true);
+
+        }
+        if (getPos == 1)
+        {
+            NightVisionPos2.SetActive(true);
+        }
+        if (getPos == 2)
+        {
+            NightVisionPos3.SetActive(true);
+        }
+        if (getPos == 3)
+        {
+            NightVisionPos4.SetActive(true);
+        }
+        if (getPos == 4)
+        {
+            NightVisionPos5.SetActive(true);
+        }
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
