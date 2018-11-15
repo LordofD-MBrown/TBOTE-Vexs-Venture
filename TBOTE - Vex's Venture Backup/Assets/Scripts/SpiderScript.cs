50f308a53fa89af703f7a60a3040447d4f351e9b
Cloudt76 <Cloudt76@users.noreply.github.com>
2018-11-14 21:06:42 -0500

    Worked on forest level
    -Added creatures by player spawn in forest level.
    -Added a mud texture as the path for the most part, still hard to notice that its the "path" , should change to stone of some type.

---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/SpiderScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderScript : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent spiderMovement;
+    Animator anim;
+    bool isWalking = false;
+
+    void Start()
+    {
+        spiderMovement = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        spiderMovement.destination = player.transform.position;
+        if (transform.position != player.transform.position)
+        {
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Torch")
+        {
+            spiderMovement.enabled = false;
+            anim.SetBool("IsDying", true);
+        }
+    }
+}
---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/WolfScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WolfScript : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent wolfMovement;
+    Animator anim;
+    bool isWalking = false;
+
+    void Start()
+    {
+        wolfMovement = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");        
+    }
+
+    void Update()
+    {
+        wolfMovement.destination = player.transform.position;
+        if (transform.position != player.transform.position)
+        {
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Torch")
+        {
+            wolfMovement.enabled = false;
+            anim.SetBool("IsDying", true);            
+        }
+    }
+
+   
+
+
+}
