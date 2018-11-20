84afebed04e6095a44198901ab9474ed2ee655f4
Cloudt76 <Cloudt76@users.noreply.github.com>
2018-11-19 20:53:52 -0500

    -Added Wolves and scripts-Zach
    -Added more wolves
    -Created script so that they find each other and then go after the player.
    -Added torches to the ruined castle, and to the gateway to the lair (very hard to see the torches unless up close with the way the darkness works.

---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/Wolf01script.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Wolf01script : MonoBehaviour {
+    GameObject player;
+    GameObject wolfobj;
+    NavMeshAgent wolfMovement;
+    Animator anim;
+    bool isWalking = false;
+    public Transform wolf;
+    int maxDist = 25;
+    int minDist = 8;
+
+    void Start()
+    {
+        wolfMovement = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+        wolfobj = GameObject.Find("wolf02");
+    }
+
+    void Update()
+    {
+        
+
+        if (Vector3.Distance(transform.position,wolfobj.transform.position)>=minDist)
+        {
+            wolfMovement.destination = wolfobj.transform.position;
+            wolfMovement.speed = 4;
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+            if (Vector3.Distance(transform.position, wolfobj.transform.position) > minDist)
+            {
+                goafterplayer();
+
+
+            }
+
+        }
+
+        // wolfMovement.destination = player.transform.position;
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Torch")
+        {
+            wolfMovement.enabled = false;
+            anim.SetBool("IsDying", true);
+        }
+    }
+
+    void goafterplayer()
+    {
+        wolfMovement.destination = player.transform.position;
+        if (transform.position != player.transform.position)
+        {
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+    }
+
+
+}
--- TBOTE - Vex's Venture Backup/Assets/Scripts/WolfScript.cs
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/WolfScript.cs
@@ -5,25 +5,41 @@ using UnityEngine.AI;
 
 public class WolfScript : MonoBehaviour {
     GameObject player;
+    GameObject wolfobj;
     NavMeshAgent wolfMovement;
     Animator anim;
     bool isWalking = false;
+    public Transform wolf;
+    int maxDist = 25;
+    int minDist = 8;
 
     void Start()
     {
         wolfMovement = GetComponent<NavMeshAgent>();
         anim = GetComponent<Animator>();
-        player = GameObject.Find("Player");        
+        player = GameObject.Find("Player");
+        wolfobj = GameObject.Find("wolf01");
     }
 
     void Update()
     {
-        wolfMovement.destination = player.transform.position;
-        if (transform.position != player.transform.position)
+        wolfMovement.destination = wolfobj.transform.position;
+        wolfMovement.speed = 4;
+        if(Vector3.Distance(transform.position, wolfobj.transform.position) >= minDist)
         {
             isWalking = true;
             anim.SetBool("IsWalking", isWalking);
+            if (Vector3.Distance(transform.position, wolfobj.transform.position) <= minDist)
+            {
+                goafterplayer();
+
+
+            }
+
         }
+
+       // wolfMovement.destination = player.transform.position;
+      
     }
     void OnTriggerEnter(Collider other)
     {
@@ -34,7 +50,15 @@ public class WolfScript : MonoBehaviour {
         }
     }
 
-   
+   void goafterplayer()
+    {
+        wolfMovement.destination = player.transform.position;
+        if (transform.position!=player.transform.position)
+        {
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+    }
 
 
 }
