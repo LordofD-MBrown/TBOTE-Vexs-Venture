f18d19735cf588dd4be6cb656b04c27c7190b236
Cloudt76 <Cloudt76@users.noreply.github.com>
2018-11-26 20:09:04 -0500

    -More Forest Enemies-Zach
    -added Rats that will chase the player only after the player gets close enough
    -Added vipers that still need some work done with the navmesh agent that causes it to jump up and down a lot.

---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/RatScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RatScript : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent ratmovement;
+    Animator anim;
+    bool isWalking = false;
+    bool isIdle = true;
+    float distFromPlayer;
+    bool dead = false;
+
+    int chaseDistIntialized = 18;
+    
+    
+    // Use this for initialization
+	void Start ()
+    {
+        ratmovement = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+
+        distFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Vector3.Distance(transform.position,player.transform.position)<=chaseDistIntialized && dead==false )
+        {
+            goafterplayer();
+        }
+        if (Vector3.Distance(transform.position, player.transform.position) > chaseDistIntialized)
+        {
+            ratmovement.enabled = false;
+            
+            isIdle = true;
+            isWalking = false;
+            anim.SetBool("IsIdle", isIdle);
+            anim.SetBool("IsWalking", isWalking);
+        }
+
+
+
+
+
+
+
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Torch")
+        {   
+            ratmovement.enabled = false;
+            dead = true;
+            anim.SetBool("IsDying", true);
+        }
+    }
+
+    void goafterplayer()
+    {
+        ratmovement.enabled = true;
+        ratmovement.speed = 5;
+        ratmovement.destination = player.transform.position;
+        if (transform.position!=player.transform.position)
+        {
+           
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+        
+
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ TBOTE - Vex's Venture Backup/Assets/Scripts/ViperScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ViperScript : MonoBehaviour {
+    GameObject player;
+    NavMeshAgent vipermovement;
+    Animator anim;
+    bool isWalking = false;
+    bool isIdle = true;
+    bool dead = false;
+
+    int chaseDistIntialized = 10;
+	// Use this for initialization
+	void Start ()
+    {
+        vipermovement = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+        player = GameObject.Find("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= chaseDistIntialized && dead == false)
+        {
+            goafterplayer();
+        }
+        if (Vector3.Distance(transform.position, player.transform.position) > chaseDistIntialized)
+        {
+            vipermovement.enabled = false;
+
+            isIdle = true;
+            isWalking = false;
+            anim.SetBool("IsIdle", isIdle);
+            anim.SetBool("IsWalking", isWalking);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Torch")
+        {
+            vipermovement.enabled = false;
+            dead = true;
+            anim.SetBool("IsDying", true);
+        }
+    }
+    void goafterplayer()
+    {
+        vipermovement.enabled = true;
+        vipermovement.speed = 5;
+        vipermovement.destination = player.transform.position;
+        if (transform.position != player.transform.position)
+        {
+
+            isWalking = true;
+            anim.SetBool("IsWalking", isWalking);
+        }
+
+
+
+    }
+
+
+
+
+
+
+
+}
